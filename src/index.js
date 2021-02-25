b480cb72ee19386776734fc5eb3b7ef2b6e9e2f8
zakharych <alexzlk@yandex.ru>
2021-02-25 13:07:40 +0300

    feat: problems solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
+exports.min = function min(array) {
+    if (array === undefined) {
+        return 0;
+    } else if (array.length < 1) {
+        return 0;
+    } else {
+        return array.sort((a, b) => {
+            return a - b;
+        })[0];
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined) {
+        return 0;
+    } else if (array.length < 1) {
+        return 0;
+    } else {
+        return array.sort((a, b) => {
+            return a - b;
+        })[array.length - 1];
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined) {
+        return 0;
+    } else if (array.length < 1) {
+        return 0;
+    } else {
+        acc = 0;
+        for (let i = 0; i < array.length; i++) {
+            acc += array[i];
+        }
+        acc = acc / array.length;
+        return acc;
+    }
+};
